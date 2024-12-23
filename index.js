6ef66c80737695aa5d424e5e9334d360538a916e
Jubayer husain <jubayerhusai360@gmail.com>
2024-12-23 12:00:04 +0600

    Get the New Tutors from client side

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const {
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.wr4sb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
@@ -34,27 +34,40 @@ async function run() {
         })
         // Creating a Database name and collection name
         const userCollection = client.db('tutorSphere').collection('users');
-        const languegesCategory= client.db('tutorSphere').collection('languegesCategory');
+        const languegesCategory = client.db('tutorSphere').collection('languegesCategory');
+        const tutorsCollection = client.db('tutorSphere').collection('tutorsCollection');
 
         // POST: Get the user from client side and Post TO database
-        app.post('/users', async(req, res)=>{
+        app.post('/users', async (req, res) => {
             const newUser = req.body;
             console.log(newUser);
             const result = await userCollection.insertOne(newUser);
-            res.send("this is a new user",result);
+            res.send("this is a new user", result);
         })
         // GET: Get the users from database
-        app.get('/users', async(req,res)=>{
+        app.get('/users', async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result)
         })
         // GET: Get the languegesCategory from database
-        app.get('/langueges', async(req,res)=>{
+        app.get('/langueges', async (req, res) => {
             const result = await languegesCategory.find().toArray();
             res.send(result)
         })
+        // POST: Get the New Tutors from client side
+        app.post('/tutors', async (req, res) => {
+            const newTutor = req.body;
+            console.log(newUser);
+            const result = await tutorsCollection.insertOne(newTutor);
+            res.send(result)
+        })
+        // GET: Get the tutors apis from database collection
+        // app.get(`/tutors`, async (req, res)=>{
+        //     const result = await tutorsCollection.find().toArray();
+        //     res.send(result)
+        // })
 
-        
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
