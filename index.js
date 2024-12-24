d1a9ff0ffb9022b4107b4837a29bd2c5236fb279
Jubayer husain <jubayerhusai360@gmail.com>
2024-12-24 19:44:50 +0600

    get the booked tutors from database

--- index.js
+++ index.js
@@ -183,6 +183,11 @@ async function run() {
             const result = await bookedTutors.insertOne(tutors);
             res.send(result)
         })
+        // GET: get the booked tutors from database
+        app.get('/booked-tutors', async(req,res)=>{
+            const result = await bookedTutors.find().toArray()
+            res.send(result)
+        })
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
